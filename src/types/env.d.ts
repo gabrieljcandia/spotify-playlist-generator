8d20571ab2652ee2f28d0250bd9a9b36d621a640
Gabriel Candia <gabrieljcandia@gmail.com>
2024-12-12 01:03:33 -0300

    improve buttons alignment

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,10 +1,10 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
-      SPOTIFY_CLIENT_ID: string;
-      SPOTIFY_CLIENT_SECRET: string;
-      SPOTIFY_REDIRECT_URI: string;
-      OPENAI_API_KEY: string;
-      SPOTIFY_REDIRECT_URI: string;
-    }
+  interface ProcessEnv {
+    SPOTIFY_CLIENT_ID: string;
+    SPOTIFY_CLIENT_SECRET: string;
+    SPOTIFY_REDIRECT_URI: string;
+    OPENAI_API_KEY: string;
+    SPOTIFY_REDIRECT_URI: string;
+    NEXT_PUBLIC_CAFECITO_APP_REDIRECT_LINK: string;
   }
-  
+}
